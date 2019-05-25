14f63b0e8350790aa4e06e1f06a4a2087589161a
Joshua Bradley <bradleyjd19@gmail.com>
2019-05-24 21:33:18 -0500

    set up database/created mysql connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "database12345",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("Error Connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as ID: " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use burgers_db;
+
+insert into burgers (burger_name, devoured)
+values 
+  ("Double Bacon Cheeseburger", false),
+  ("Whiskey BBQ Burger", false),
+  ("White Spot Gus Burger", false);
